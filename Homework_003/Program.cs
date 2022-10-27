4c782105aa4e67e739a74b8edc4486262df7e971
Viktoriya_Morozowa <victoriya.morozowa1997@yandex.ru>
2022-10-27 22:38:40 +0300

    task 6

---
+++ Homework_003/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter number");
+int x = Convert.ToInt32(Console.ReadLine());
+bool isEven = x % 2 == 0;
+if (isEven) {
+    Console.WriteLine("Even Number");
+} else {
+    Console.WriteLine("Odd Number");
+}
