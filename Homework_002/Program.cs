891fa8d5442873d347ac37ef83dbb8077609f406
Viktoriya_Morozowa <victoriya.morozowa1997@yandex.ru>
2022-10-27 22:32:53 +0300

    task 4

---
+++ Homework_002/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter b");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter c");
+int c = Convert.ToInt32(Console.ReadLine());
+int max;
+if (a > b) {
+    max = a;
+} else {
+    max = b;
+}
+if (c > max) {
+    max = c;
+}
+Console.WriteLine($"Max = {max}");
+
+
