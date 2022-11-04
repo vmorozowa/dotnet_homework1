aa029cc549eceeb5d7e50b19dec186099052b2cc
Viktoriya_Morozowa <victoriya.morozowa1997@yandex.ru>
2022-11-04 11:15:38 +0300

    task 13

---
+++ Homework_006/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter number");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x < 100) {
+    Console.WriteLine("третьей цифры нет");
+} else {
+    Console.WriteLine(x.ToString()[2]);
+}
