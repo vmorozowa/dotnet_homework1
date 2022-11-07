40eda198322a2eb5e46ec0fef096d94d79732288
Viktoriya_Morozowa <victoriya.morozowa1997@yandex.ru>
2022-11-07 15:43:31 +0300

    task 23

---
+++ Homework_010/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter N");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while(i <= n) {
+    double res = Math.Pow(i, 3);
+    Console.WriteLine(res);
+    i = i + 1;
+}
