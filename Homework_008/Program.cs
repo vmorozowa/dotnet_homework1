73474fb2d13298fd5a93e533d7a745d6541d2aab
Viktoriya_Morozowa <victoriya.morozowa1997@yandex.ru>
2022-11-07 15:34:57 +0300

    task 19

---
+++ Homework_008/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter number");
+int x = Convert.ToInt32(Console.ReadLine());
+string s = x.ToString();
+if (s[0] == s[4] && s[1] == s[3]) {
+    Console.WriteLine("да");
+} else {
+    Console.WriteLine("нет");
+}
