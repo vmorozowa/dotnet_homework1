e5929c449b6641af0c9ab4ef19ba6b8805eb40ef
Viktoriya_Morozowa <victoriya.morozowa1997@yandex.ru>
2022-11-08 16:30:42 +0300

    task 25

---
+++ Homework_011/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter B");
+int b = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int res = a;
+while (i < b) {
+    res = res * a;
+    i++;
+}
+Console.WriteLine(res);
