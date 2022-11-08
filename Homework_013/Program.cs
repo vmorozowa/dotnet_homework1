61e54c9dedc444b21155134bdb4404cbfe8f2115
Viktoriya_Morozowa <victoriya.morozowa1997@yandex.ru>
2022-11-08 16:38:39 +0300

    task 29

---
+++ Homework_013/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter N");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int[] array = new int[n];
+while (i < n) {
+    Console.WriteLine("Enter number");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    i++;
+}
+Console.WriteLine("[{0}]", string.Join(", ", array));
