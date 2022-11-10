12834e9570aae037fab60ce6014df6c4b2ab967f
Viktoriya_Morozowa <victoriya.morozowa1997@yandex.ru>
2022-11-10 18:47:24 +0300

    task 34

---
+++ Homework_014/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter N");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int[] array = new int[n];
+Random rnd = new Random();
+while (i < n) {
+    array[i] = rnd.Next(100, 999);
+    i++;
+}
+int j = 0;
+int count = 0;
+while (j < n) {
+    if (array[j] % 2 == 0) {
+        count++;
+    }
+    j++;
+}
+Console.WriteLine("[{0}]", string.Join(", ", array));
+Console.WriteLine(count);
