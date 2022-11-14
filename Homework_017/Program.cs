78c431dcb2035b4229e7fe4268db8cf39b99ee6a
Viktoriya_Morozowa <victoriya.morozowa1997@yandex.ru>
2022-11-14 16:47:11 +0300

    task 64

---
+++ Homework_017/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter N");
+int n = Convert.ToInt32(Console.ReadLine());
+string result = getNumbers(n, "");
+Console.WriteLine(result);
+
+
+string getNumbers(int x, string acc) {
+    if (x == 1) {
+        return acc + x.ToString();
+    }
+    acc = acc + x.ToString() + ", ";
+    return getNumbers(x - 1, acc);
+}
