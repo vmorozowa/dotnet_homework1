0e49acf94ffe165bf7354a5507f6bfb24797d2dc
Viktoriya_Morozowa <victoriya.morozowa1997@yandex.ru>
2022-11-14 16:50:22 +0300

    task 66

---
+++ Homework_018/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter N");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (m <= n) {
+    sum = sum + m;
+    m++;
+}
+Console.WriteLine(sum);
